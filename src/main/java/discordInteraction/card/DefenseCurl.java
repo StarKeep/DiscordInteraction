4cd1cb30b4a092e41f29123ca4285240e43db4b1
StarKelp <fergusonstf@yahoo.com>
2021-03-18 15:26:50 -0500

    Added Defense Curl

---
+++ src/main/java/discordInteraction/card/DefenseCurl.java
@@ -0,0 +1,43 @@
+package discordInteraction.card;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+import discordInteraction.FlavorType;
+import discordInteraction.Utilities;
+import discordInteraction.command.Result;
+import net.dv8tion.jda.api.entities.User;
+
+public class DefenseCurl extends CardTargetless {
+    @Override
+    public String getName() {
+        return "Defense Curl";
+    }
+
+    @Override
+    public int getCost() {
+        return 6;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Apply Intangible 3 to the player.";
+    }
+
+    @Override
+    public String getFlavorText() {
+        return "Gotta protect those squishy inner bits.";
+    }
+
+    @Override
+    public FlavorType[] getFlavorTypes() {
+        return new FlavorType[]{
+                FlavorType.support
+        };
+    }
+
+    @Override
+    public Result activate(User user, AbstractPlayer player) {
+        Utilities.applyPower(player, new IntangiblePlayerPower(player, 3));
+        return new Result(true, "You've applied 3 stacks of Intangible to the player.");
+    }
+}
